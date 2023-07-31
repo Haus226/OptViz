65f41d714b112614d09cefe38ebd4081fff6a9e3
Haus226 <100110611+Haus226@users.noreply.github.com>
2023-07-31 09:59:59 +0800

    Update GIF and PNG files and main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import matplotlib.pyplot
-
 import graDescending
 from Var import Var, draw_dag
 import ParserPostfix
@@ -33,70 +32,60 @@ name = [
     "rosenbrock"
 ]
 
-
-
 # Test (Visualize DAG and AST)
-for t, i, n in zip(func, init, name):
-    print(n)
-    variables = i.copy()
-    # Prepare Var objects
-    for v in variables:
-        variables[v] = Var(i[v], exp=v, type="var", info=v)
-    ast = ParserAST.parse(t)
-    ParserAST.draw_ast(ast, n + "_AST")
-    _ = ParserAST.evaluateVar(ast, variables)
-    # Perform backward propagation to calculate the gradient
-    _.backward()
-    draw_dag(_, n + "_DAG_AST", "TB")
-
-for t, i, n in zip(func, init, name):
-    print(n)
-    variables = i.copy()
-    # Prepare Var objects
-    for v in variables:
-        variables[v] = Var(i[v], exp=v, type="var", info=v)
-    tokens = ParserPostfix.ShuntingYard(t)
-    _ = ParserPostfix.evaluateVar(tokens, variables)
-    # Perform backward propagation to calculate the gradient
-    _.backward()
-    draw_dag(_, n + "_DAG_PF", "TB")
+# for t, i, n in zip(func, init, name):
+#     print(n)
+#     variables = i.copy()
+#     # Prepare Var objects
+#     for v in variables:
+#         variables[v] = Var(i[v], exp=v, type="var", info=v)
+#     ast = ParserAST.parse(t)
+#     ParserAST.draw_ast(ast, n + "_AST")
+#     _ = ParserAST.evaluateVar(ast, variables)
+#     # Perform backward propagation to calculate the gradient
+#     _.backward()
+#     draw_dag(_, n + "_DAG_AST", "TB")
+# for t, i, n in zip(func, init, name):
+#     print(n)
+#     variables = i.copy()
+#     # Prepare Var objects
+#     for v in variables:
+#         variables[v] = Var(i[v], exp=v, type="var", info=v)
+#     tokens = ParserPostfix.ShuntingYard(t)
+#     _ = ParserPostfix.evaluateVar(tokens, variables)
+#     # Perform backward propagation to calculate the gradient
+#     _.backward()
+#     draw_dag(_, n + "_DAG_PF", "TB")
 
 
 # Test (Optimize functions using different Gradient Descend based method)
 import time
-iter = 50
+iter = 100
 b = time.time()
 for t, i, n in zip(func, init, name):
     print("-" * 50 + n + "-" * 50)
     f_var, f = ParserPostfix.expression_to_function(t)
-
-
     print("Vanilla")
     coords, evaluated = graDescending.GradientDescending(f_var, i, lr=0.01, iter=iter)
-    graDescending.animate(coords, f, title=f"Vanilla_{n}", filename=f"Vanilla_{n}.gif")
+    graDescending.animate(coords, f, title=f"Vanilla_{n}", filename=f"Vanilla_{n}.png")
     # print(coords[-10:-1])
-
     print("Adam")
-    coords, evaluated = graDescending.Adam(f_var, i, lr=0.01, iter=iter)
-    graDescending. animate(coords, f, title=f"Adam_{n}", filename=f"Adam_{n}.gif")
+    coords, evaluated = graDescending.Adam(f_var, i, lr=0.1, iter=iter)
+    graDescending. animate(coords, f, title=f"Adam_{n}", filename=f"Adam_{n}.png")
     # print(coords[-10:-1])
-
     print("RMSprop")
-    coords, evaluated = graDescending.RMSprop(f_var, i, lr=0.01, iter=iter)
-    graDescending.animate(coords, f, title=f"RMSprop_{n}", filename=f"RMSprop_{n}.gif")
+    coords, evaluated = graDescending.RMSprop(f_var, i, lr=0.1, iter=iter)
+    graDescending.animate(coords, f, title=f"RMSprop_{n}", filename=f"RMSprop_{n}.png")
     # print(coords[-10:-1])
-
     print("Momentum")
     coords, evaluated = graDescending.Momentum(f_var, i, lr=0.001, iter=iter)
-    graDescending.animate(coords, f, title=f"Momentum_{n}", filename=f"Momentum_{n}.gif")
+    graDescending.animate(coords, f, title=f"Momentum_{n}", filename=f"Momentum_{n}.png")
     # print(coords[-10:-1])
-
     print("BFGS")
     coords, evaluated = graDescending.BFGS(f_var, i, iter=iter)
-    graDescending.animate(coords, f, title=f"BFGS_{n}", filename=f"BFGS_{n}.gif")
+    graDescending.animate(coords, f, title=f"BFGS_{n}", filename=f"BFGS_{n}.png")
     # print(coords[-10:-1])
     matplotlib.pyplot.close()
-
 e = time.time()
 print(e - b)
 #
